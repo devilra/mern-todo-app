eed1f5811dde7224048caee037261498b4da6189
devilra <rockerraja906@gmail.com>
2025-07-11 21:04:54 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,13 @@ const taskRoutes = require("./routes/todoRoute");
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use("/api", taskRoutes);
+
+app.get("/health", (req, res) => {
+  res.send("Server is healthy!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server started ${PORT}`);
